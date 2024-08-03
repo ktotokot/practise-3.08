8018ca1d9c961eca0c96153bec508edea6cc80d4
ktotokot <shabuninae@gmail.com>
2024-08-03 15:27:38 +0300

    form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,37 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
+import { useState } from 'react';
 
-export const Form = () => {
-  return <h2>Form</h2>;
+export const Form = ({onSubmit}) => {
+  const [searchQuery, setSearchQuery] = useState('')
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value)
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!searchQuery.trim()){
+      return alert('cant be empty')
+    }
+    onSubmit(searchQuery)
+    setSearchQuery('')
+  }
+
+  
+  return <form className={style.form}
+  onSubmit={handleSubmit}>
+  <button className={style.button} type="submit">
+    <FiSearch size="16px" />
+  </button>
+
+  <input
+  onChange={handleChange}
+  value={searchQuery}
+    className={style.input}
+    placeholder="What do you want to write?"
+    name="search"
+    required
+    autoFocus
+  />
+</form>;
 };
